572decada6e798de27454c39673f74b4549b5130
solvman <solvman@gmail.com>
2024-09-02 22:01:38 -0500

    chore: Update layout and remove unused files

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,28 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
+import { cn } from "@/utils/utils";
 import data from "../data/notifications.json";
+import Link from "next/link";
 
 type Notification = {
   id: number;
-  avatar: {
-    src: string;
+  user: {
+    name: string;
+    href: string;
+    avatar: {
+      src: string;
+    };
   };
-  name: string;
   action: string;
   link?: {
     text: string;
     url: string;
   };
   time: string;
-  isRead: boolean;
+  isUnread: boolean;
   message?: string;
   image?: {
     src: string;
@@ -30,45 +35,108 @@ export default function Home() {
     data.notifications,
   );
 
+  const unreadNotifications = notifications.filter(
+    (notification) => notification.isUnread,
+  ).length;
+
+  function markAsRead(id: number) {
+    setNotifications((notifications) =>
+      notifications.map((notification) =>
+        notification.id === id
+          ? { ...notification, isUnread: false }
+          : notification,
+      ),
+    );
+  }
+
+  function markAllAsRead() {
+    setNotifications((notifications) =>
+      notifications.map((notification) => ({
+        ...notification,
+        isUnread: false,
+      })),
+    );
+  }
+
   return (
-    <main>
-      <h1>Notifications</h1>
-      <ul>
-        {notifications.map((notification) => (
-          <li key={notification.id} className="flex flex-row gap-x-3 p-4">
-            <Image
-              src={notification.avatar.src}
-              alt={notification.name}
-              width={50}
-              height={50}
-              className="h-10 w-10"
-            />
-            <div className="flex-1">
-              <p>
-                <span>{notification.name}</span>{" "}
-                <span>{notification.action}</span>{" "}
-                {notification.link && (
-                  <a href={notification.link.url}>{notification.link.text}</a>
-                )}
-                {!notification.isRead && (
-                  <span className="block h-2 w-2 rounded-full bg-red-500"></span>
-                )}
-              </p>
-              <time>{notification.time}</time>
-              {notification.message && <p>{notification.message}</p>}
-            </div>
-            {notification.image && (
+    <main className="md:grid md:h-screen md:place-items-center">
+      <h1 className="sr-only">Notification page project</h1>
+      <article className="bg-site-white p-4 md:max-w-[730px] md:rounded-2xl md:shadow-2xl">
+        <header className="flex flex-row gap-2 py-6">
+          <h2 className="text-site-clamp-20px-24px text-site-very-dark-blue font-extrabold">
+            Notifications
+          </h2>
+          <span className="bg-site-blue text-site-white flex flex-row items-center justify-center rounded-md px-3 font-extrabold">
+            {unreadNotifications}
+          </span>
+          <button
+            onClick={markAllAsRead}
+            className="text-site-dark-grey-blue hover:text-site-blue ml-auto font-medium transition-colors duration-200 ease-in-out"
+          >
+            Mark all as read
+          </button>
+        </header>
+        <ul className="flex flex-col gap-4">
+          {notifications.map((notification) => (
+            <li
+              key={notification.id}
+              className={cn(
+                "flex flex-row gap-x-3 rounded-lg p-4 transition-colors duration-200 ease-in-out",
+                notification.isUnread && "bg-site-snow cursor-pointer",
+              )}
+              onClick={() => markAsRead(notification.id)}
+            >
               <Image
-                src={notification.image.src}
-                alt={notification.image.alt}
+                src={notification.user.avatar.src}
+                alt={notification.user.name}
                 width={50}
                 height={50}
-                className="h-10 w-10"
+                className="w-site-clamp-39px-45px h-site-clamp-39px-45px"
               />
-            )}
-          </li>
-        ))}
-      </ul>
+              <div className="flex-1">
+                <p>
+                  <a
+                    className="text-site-very-dark-blue hover:text-site-blue font-extrabold"
+                    href={notification.user.href}
+                  >
+                    {notification.user.name}
+                  </a>{" "}
+                  <span>{notification.action}</span>
+                  {notification.link && (
+                    <>
+                      {" "}
+                      <Link
+                        href={notification.link.url}
+                        className="hover:text-site-blue font-bold transition-colors duration-200 ease-in-out"
+                      >
+                        {notification.link.text}
+                      </Link>
+                    </>
+                  )}
+                  {notification.isUnread && (
+                    <span className="ml-1 inline-block h-2 w-2 rounded-full bg-red-500"></span>
+                  )}
+                </p>
+                <time className="text-site-grey-blue">{notification.time}</time>
+                {notification.message && (
+                  <p className="border-site-light-grey-blue hover:bg-site-light-grey-blue hover:border-site-very-light-grey-blue mt-3 cursor-pointer rounded-md border p-4 transition-colors duration-200 ease-in-out">
+                    {notification.message}
+                  </p>
+                )}
+              </div>
+              {notification.image && (
+                <Image
+                  src={notification.image.src}
+                  alt={notification.image.alt}
+                  width={50}
+                  height={50}
+                  className="w-site-clamp-39px-45px h-site-clamp-39px-45px"
+                />
+              )}
+            </li>
+          ))}
+        </ul>
+      </article>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      height: {
+        "site-clamp-39px-45px":
+          "clamp(2.4375rem, 1.9068rem + 2.2642vw, 2.8125rem)",
+      },
+      width: {
+        "site-clamp-39px-45px":
+          "clamp(2.4375rem, 1.9068rem + 2.2642vw, 2.8125rem)",
+      },
+      fontSize: {
+        "site-clapm-14px-16px": "clamp(0.875rem, 0.6981rem + 0.7547vw, 1rem)",
+        "site-clamp-20px-24px": "clamp(1.25rem, 0.8962rem + 1.5094vw, 1.5rem)",
+      },
+      colors: {
+        site: {
+          red: "hsl(1 90% 64% / <alpha-value>)",
+          blue: "hsl(219 85% 26% / <alpha-value>)",
+          white: "hsl(0 0% 100% / <alpha-value>)",
+          snow: "hsl(210 60% 98% / <alpha-value>)",
+          "light-grey-blue": "hsl(211 68% 94% / <alpha-value>)",
+          "very-light-grey-blue": "hsl(205 33% 90% / <alpha-value>)",
+          "grey-blue": "hsl(219 14% 63% / <alpha-value>)",
+          "dark-grey-blue": "hsl(219 12% 42% / <alpha-value>)",
+          "very-dark-blue": "hsl(224 21% 14% / <alpha-value>)",
+        },
       },
     },
   },
